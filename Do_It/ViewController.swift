648297212749723688c52b8ddbe7492600809ff5
Brandon Noecker <brandonnoecker@me.com>
2017-08-16 01:13:56 -0400

    Initial Commit

---
+++ Do_It/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Do_It
+//
+//  Created by Brandon Noecker on 8/16/17.
+//  Copyright © 2017 HypeSyde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
